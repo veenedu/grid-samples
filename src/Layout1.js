83b15a5f825a099b4291669b0d95fef3a6fd58ef
Praveen Prasad <praveenprasad@Praveens-MacBook-Pro.local>
2017-02-07 13:19:38 +0530

    Update Layout1

--- src/Layout1.js
+++ src/Layout1.js
@@ -7,7 +7,38 @@ import 'veenbox/dist/css-reset.css';
 function getWords(n){
     return faker.lorem.words(n)
 }
-class GeekyAntGrid extends Component {
+
+let styles= {
+    gray1:{
+        backgroundColor:'#333'
+    },
+    gray2:{
+        backgroundColor:'#777'
+    },
+    gray3:{
+        backgroundColor:'#999'
+    },
+    gray4:{
+        backgroundColor:'#aaa'
+    },
+    green:{
+        backgroundColor:'green'
+    },
+    blue:{
+       backgroundColor:'blue' 
+    },
+    yellow:{
+       backgroundColor:'yellow' 
+    },
+    teal:{
+        backgroundColor:'teal'
+    },
+    pink:{
+        backgroundColor:'pink'
+    }  
+}
+
+class Layout1 extends Component {
     render() {
         return (
              <Grid style={{
@@ -15,28 +46,28 @@ class GeekyAntGrid extends Component {
              }}>
                     <Col style={{backgroundColor:'red'}}>
                         <Grid>
-                            <Col style={{width:300,backgroundColor:'#333'}}></Col>
-                            <Col  style={{backgroundColor:'#777'}}></Col>
-                            <Col  style={{backgroundColor:'#aaa'}}></Col>
+                            <Col style={ [styles.gray1, {width:300}]}></Col>
+                            <Col style={styles.gray2}></Col>
+                            <Col style={styles.gray3}></Col>
                         </Grid>
                     </Col>
                     <Col>
                         <Grid>
-                            <Row size={50} style={{backgroundColor:'green'}}>
+                            <Row size={50} style={styles.green}>
                                 <Grid>
-                                    <Col style={{ width: 140, backgroundColor:'green' }}> Fixed width </Col>
-                                    <Col style={{  backgroundColor:'#999' }}> Fluid width </Col>
+                                    <Col style={[{ width: 140 },styles.green]}> Fixed width </Col>
+                                    <Col style={styles.gray4}> Fluid width </Col>
                                 </Grid>
                             </Row>
-                            <Row size={50} style={{backgroundColor:'blue'}}>
+                            <Row size={50} style={styles.blue}>
                                 <Grid>
-                                    <Row  style={{height:50,backgroundColor:'yellow'}}></Row>
+                                    <Row  style={[{height:50},styles.gray1]}></Row>
                                     <Row>
-                                        <ScrollView style={{backgroundColor:'teal'}}>
+                                        <ScrollView style={styles.teal}>
                                             {getWords(300)}
                                         </ScrollView>                                        
                                     </Row>
-                                    <Row   style={{height:50,backgroundColor:'pink'}}></Row>
+                                    <Row   style={[{height:50},styles.pink]}></Row>
                                 </Grid>
                             </Row>
                         </Grid>
@@ -46,4 +77,4 @@ class GeekyAntGrid extends Component {
     }
 }
 
-export default GeekyAntGrid;
+export default Layout1;
